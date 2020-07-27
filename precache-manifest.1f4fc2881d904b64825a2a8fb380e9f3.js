61fac6cbc3fe5bcf3bbe5d92fe57b2168d8482a2
Raymond Oh <raymond.oh@gmail.com>
2020-07-27 16:34:41 -0400

    Updates

---
+++ precache-manifest.1f4fc2881d904b64825a2a8fb380e9f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe250e197e7c80b3344cd9f2d0050362",
+    "url": "/ToDoApp-React/index.html"
+  },
+  {
+    "revision": "4f91f83531fb1c0beccd",
+    "url": "/ToDoApp-React/static/css/main.fd5cb9d6.chunk.css"
+  },
+  {
+    "revision": "a66a7297d82740c5186a",
+    "url": "/ToDoApp-React/static/js/2.817df802.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoApp-React/static/js/2.817df802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f91f83531fb1c0beccd",
+    "url": "/ToDoApp-React/static/js/main.507fa19f.chunk.js"
+  },
+  {
+    "revision": "1aa563e62d4df2bc6e3c",
+    "url": "/ToDoApp-React/static/js/runtime-main.fcffa9ed.js"
+  }
+]);
